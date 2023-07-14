68beec8010c7e977795e39cfb420c4916acb4ec7
siri <svemula1s@semo.edu>
2023-07-14 09:24:56 -0400

    i am sending new files

---
+++ conditions .py
@@ -0,0 +1,13 @@
+#brand =input("enter your fav brand:")
+#if brand == "puma":
+ #   print("good nice brand")
+#elif brand =="us brand":
+ #   print("indians not bear the charges")
+#else:
+ #   print("you have to choose one brand")
+
+s =input("enter the string")
+i = 0
+for x in s:
+    print("the charchter at index",i,"and actualchar is" ,x)
+    i+=1
---
+++ function.py
@@ -0,0 +1,7 @@
+def loop():
+    for hurry_counter in range(1,4):
+    for hip_counter in range(1,3):
+        print('hip')
+
+        print('hurry')
+ loop()       
